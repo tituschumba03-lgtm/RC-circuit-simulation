010385b16c658cbee6fa95869577d592d08a28e1
Talim <tituschumba03@gmail.com>
2026-02-01 17:27:31 +0300

    Update RC Circuit Simulation.py

--- RC Circuit Simulation.py
+++ RC Circuit Simulation.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-R = 1000      # Ohms
-C = 1e-6      # Farads
-V = 5         # Volts
+R = 1000      # Resistance
+C = 1e-6      # Capacitance
+V = 5         # Voltage
 
-t = np.linspace(0, 0.01, 1000)                   #
-Vc = V * (1 - np.exp(-t / (R * C)))              #
+t = np.linspace(0, 0.01, 1000)                   
+Vc = V * ( np.exp(-t / (R * C)))              
 
-plt.plot(t, Vc)                                  #
-plt.xlabel("Time (s)")                           #
-plt.ylabel("Voltage (V)")                        #
-plt.title("RC Charging Curve")                  #
-plt.show()                                       #
+plt.plot(t, Vc)                                  
+plt.xlabel("Time (s)")                           
+plt.ylabel("Voltage (V)")                        
+plt.title("RC discharging Curve")                  
+plt.show()                                       
+
